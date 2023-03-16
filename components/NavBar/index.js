5de89203348b3997913147633b45ab3dee8dc6e1
lance-malaga <lmalaga@my.bcit.ca>
2023-03-15 22:47:18 -0700

    add initial navbar component

---
+++ components/NavBar/index.js
@@ -0,0 +1,11 @@
+import styles from './NavBar.module.css'
+
+export default function NavBar() {
+    return (
+        <>
+            <nav className={styles.navbar}>
+                
+            </nav>
+        </>
+    )
+}
