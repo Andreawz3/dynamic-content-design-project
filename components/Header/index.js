6fe2e7b9dd709dc97d37e60dc9a06ad306060973
Andrea Wu <andrea.wzg@gmail.com>
2023-03-19 14:53:44 -0700

    add public folder, add title and site logo, and add 2 more colours and add 'lato' and 'capuche' fonts in globals.css file

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head"
 
 export default function Header({
-    title = "",
+    title = "Yori",
     metaTitle = "",
     metaDescription = "",
 }) {
@@ -12,7 +12,8 @@ export default function Header({
             <meta property="title"  content={metaTitle} />
             <meta property="description"  content={metaDescription} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="" />
+            <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet"></link>
+            <link rel="icon" href="/site-logo.png" />
         </Head>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 // components
 import Header from '@/components/Header'
 import styles from '@/styles/Home.module.css'
+import NavBar from '@/components/NavBar'
 
 export default function Home() {
 	return (
 		<>
-			<Header />
+			<Header/>
 			<main className={styles.main}>
-				
+				<NavBar/>
 			</main>
 		</>
 	)
