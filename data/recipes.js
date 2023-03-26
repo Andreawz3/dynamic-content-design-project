30a34ab04f8cba7521a3f9b064fb62238e36e80b
Andrea Wu <andrea.wzg@gmail.com>
2023-03-26 12:41:50 -0700

    create data folder and add recipe.js file

---
+++ data/recipes.js
@@ -0,0 +1,14 @@
+export const inventory = {
+    recipe: [
+        {
+            name: "",
+            timeToPrepare: "",
+            timeToCook: "",
+            category:"",
+            level: "",
+            ingredients: [],
+            direction:[],
+            review:""
+        }
+    ]
+}
