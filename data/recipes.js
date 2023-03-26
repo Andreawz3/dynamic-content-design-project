bb36c0ff9505d1e1b8bd5cf7d84c6dddfd02ce15
Juliet <113483666+juliet-jskim@users.noreply.github.com>
2023-03-26 13:07:53 -0700

    added data to the recipe.js file, deleted .json file

--- data/recipes.js
+++ data/recipes.js
@@ -1,14 +1,49 @@
 export const inventory = {
     recipe: [
         {
-            name: "",
-            timeToPrepare: "",
-            timeToCook: "",
-            category:"",
-            level: "",
-            ingredients: [],
-            direction:[],
-            review:""
+            "dishName": "Homemade Mandu",
+            "prepTime":"30 min",
+            "cookTime":"7 min",
+            "cuisineCategory":"Korean",
+            "difficulty":"Medium",
+            "ingredientList": ["1 cup lean ground pork",
+            "1 cup chopped cabbage",
+            "1/2 cup chopped scallions",
+            "1/2 cup chopped onion",
+            "1/2 cup chopped carrot",
+            "1 tablespoon minced garlic",
+            "1 tablespoon soy sauce",
+            "1 tablespoon sesame oil",
+            "1/4 teaspoon black pepper",
+            "1/4 teaspoon salt",
+            "1 package of round mandu wrappers (or you can make your own)"],
+            "directions":["1. In a large mixing bowl, combine the ground pork, cabbage, scallions, onion, carrot, garlic, soy sauce, sesame oil, black pepper, and salt. Mix well until all the ingredients are evenly distributed.",
+            "2. Take one mandu wrapper and place about 1 tablespoon of filling in the center. Fold the wrapper in half to create a half-moon shape, and then press the edges together to seal. You can also crimp the edges with a fork for a decorative touch.",
+            "3. Repeat the process with the remaining wrappers and filling until all the filling is used up.",
+            "4. Heat a large non-stick pan over medium-high heat. Once the pan is hot, add the mandu in a single layer and cook for about 2-3 minutes, or until the bottoms are golden brown.",
+            "5. Pour in 1/4 cup of water into the pan, then quickly cover with a lid to create steam. Cook for an additional 2-3 minutes or until the mandu are cooked through.",
+            "6. Serve hot with a side of dipping sauce made with soy sauce, rice vinegar, and a little bit of sugar."],
+            "review":"4"
+        },
+        {
+            "dishName": "Korean Bibimbap (Mixed Rice Bowl)",
+            "prepTime":"45 min",
+            "cookTime":"15 min",
+            "cuisineCategory":"Korean",
+            "difficulty":"Hard",
+            "ingredientList":["1 cup cooked brown rice","1/2 cup spinach, blanched and squeezed dry","1/2 cup bean sprouts, blanched and squeezed dry","1/2 cup carrots, julienned and sautéed","1/2 cup shiitake mushrooms, sautéed","1/2 cup ground beef, sautéed with soy sauce and garlic","1 egg fried or poached","Sesame oil","Soy sauce","Gochujang (Korean chili paste)","Toasted sesame seeds"],
+            "directions":["1. Prepare all the ingredients as directed and set them aside.", "2. In a small bowl, mix together 1 tablespoon of sesame oil and 1 tablespoon of soy sauce. Drizzle the mixture over the cooked brown rice and mix well.","3. Arrange the prepared vegetables and beef on top of the rice in a circular pattern. Top with the fried or poached egg in the center.","5. Drizzle a little bit of gochujang on top of the egg and sprinkle with toasted sesame seeds.","Serve immediately and mix everything together before eating."],
+            "review":"5"
+        },
+        {
+            "dishName": "",
+            "prepTime":"",
+            "cookTime":"",
+            "cuisineCategory":"",
+            "difficulty":"",
+            "ingredientList":"",
+            "directions":"",
+            "review":""
         }
     ]
 }
