3f61af2acf75d80f15545355270451d7045ac179
Juliet <113483666+juliet-jskim@users.noreply.github.com>
2023-03-25 20:08:43 -0700

    added navbar icons

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,10 +1,13 @@
 import styles from './NavBar.module.css'
+import Image from '../../public/assets/navbar-icons'
 
 export default function NavBar() {
     return (
         <>
             <nav className={styles.navbar}>
-                
+                <div className={styles.home__icon}>
+                    <Image src=''></Image>
+                </div>
             </nav>
         </>
     )
