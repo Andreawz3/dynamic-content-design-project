47de0df18372e85579986335c2b580d4dcff29b7
lance-malaga <lmalaga@my.bcit.ca>
2023-03-30 08:34:40 -0700

    update landing page, header, recipes data

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 // components
 import Header from '@/components/Header'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Landing.module.css'
 import NavBar from '@/components/NavBar'
 import Logo from '@/components/Logo'
 import Button from '@/components/Button'
 import Image from 'next/image'
 
-export default function Home() {
+export default function Landing() {
+
 	return (
 		<>
+			<Header 
+				title='Yori - Landing Page'
+				metaTitle='Yori - Landing Page'
+			/>
 			<main className={styles.main}>
 				{/* <Image /> */}
-				<h1>Nutritiously Good</h1>
-				<p>Discover the right meals for a healthy lifestyle.</p>
+				<h1>The Taste Of Healthy Living</h1>
+				<p>Fuel your body and soul with our deliciously easy and healthy recipes.</p>
 				<button>
 					{/* <Image /> */}
 				</button>
