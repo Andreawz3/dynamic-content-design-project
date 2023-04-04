188774e3524bdb7d58f82214f2578f1642127c87
Juliet <113483666+juliet-jskim@users.noreply.github.com>
2023-04-03 18:40:58 -0700

    added modifications to navbar and home.js

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,42 +1,42 @@
-import styles from './NavBar.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
+import styles from "./NavBar.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function NavBar() {
-    return (
-        <>
-            <nav className={styles.navbar}>
-                <div className={styles.home__icon}>
-                    <Link href='/'>
-                        <Image 
-                            src='/assets/navbar-icons/home-primary.png'
-                            alt={'home'}
-                            height={40}
-                            width={40}
-                            />
-                    </Link>
-                </div>
-                <div className={styles.favourites__icon}>
-                    <Link href='/favourites'>
-                        <Image src='/assets/navbar-icons/fave-primary.png'
-                        alt={'favourites'}
-                        height={40}
-                        width={40}
-                        />
-                    </Link>
-                   
-                </div>
-                <div className={styles.list__icon}>
-                    <Link href='/shoppingList'>
-                        <Image src='/assets/navbar-icons/notes-primary.png'
-                        alt={'list'}
-                        height={40}
-                        width={40}
-                        />
-                    </Link>
-                  
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.home__icon}>
+          <Link href="/">
+            <Image
+              src="/assets/navbar-icons/home-primary.png"
+              alt={"home"}
+              height={40}
+              width={40}
+            />
+          </Link>
+        </div>
+        <div className={styles.favourites__icon}>
+          <Link href="/favourites">
+            <Image
+              src="/assets/navbar-icons/fave-primary.png"
+              alt={"favourites"}
+              height={40}
+              width={40}
+            />
+          </Link>
+        </div>
+        <div className={styles.list__icon}>
+          <Link href="/shoppingList">
+            <Image
+              src="/assets/navbar-icons/notes-primary.png"
+              alt={"list"}
+              height={40}
+              width={40}
+            />
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
 }
--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,10 @@
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
     return (
         <>
             home page
+            <NavBar/>
         </>
     )
 }
