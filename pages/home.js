c8b1d6a2bec817489610e29b3bbadd425747c836
lance-malaga <lmalaga@my.bcit.ca>
2023-04-08 13:48:20 -0700

    initial commit home page

--- pages/home.js
+++ pages/home.js
@@ -11,9 +11,24 @@ export default function Home() {
                 title={'Home'}
                 metaTitle={'Home'}
             />
-            <main className={styles.home__page}>
+            <main className={styles.home_page}>
                 <TopNav/>
+                <div className={styles.home_page__greeting}>
+                    <h1>Hello, Jane</h1>
+                    <p>Want to eat some healthy meals?</p>
+                </div>
+                <div className={styles.home_page__filter}>
 
+                </div>
+                <div className={styles.filter__slider}>
+
+                </div>
+                <div className={styles.home_page__carousel}>
+
+                </div>
+                <div className={styles.home_page__categories}>
+
+                </div>
             </main>
         </>
     )
