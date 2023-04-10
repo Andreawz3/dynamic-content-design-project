199dd46a7964f3c2f6c8bf72814407e588f35526
lance-malaga <lmalaga@my.bcit.ca>
2023-04-10 09:23:58 -0700

    add initial home layout

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,34 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import styles from '@/styles/Home.module.css'
 
 // components
+import Search from '@/components/Search';
 import TopNav from "@/components/TopNav";
 import Header from '@/components/Header';
 import Button from '@/components/Button';
-import { FilterIcon, SearchIcon } from '@/public/icons/icons';
+import FilterMenu from '@/components/FilterMenu';
 
+// data
+import { inventory } from '@/data/recipes';
 
 export default function Home() {
+    const [recipeData, setRecipeData] = useState(inventory.recipe);
+    const [currentFilter, setCurrentFilter] = useState("");
+    const filteredCategory = [];
+    
+    recipeData.forEach((value) => {
+        if (!filteredCategory.includes(value.category)) {
+            filteredCategory.push(value.category);
+		}
+	});
+    
+    const filterMenu = filteredCategory.map((country) => {
+        return {
+            country: country,
+        }
+    })
+
     return (
         <>
             <Header
@@ -22,31 +42,45 @@ export default function Home() {
                     <p>Want to eat some healthy meals?</p>
                 </div>
                 <div className={styles.home_page__search}>
-                    <div className={styles.search__container}>
-                        <div className={styles.search__icon}>
-                            <SearchIcon />
-                        </div>
-                        <input
-                            className={styles.search__input}
-                            placeholder="Search"
-                            type="text"
-                            // onChange={onChange}
-                            // onFocus={onFocus}
-                            // value={query}
-                        />
-                        <div className={styles.filter__icon}>
-                            <FilterIcon/>
-                        </div>
-                    </div>
+                    <Search />
                 </div>
                 <div className={styles.home_page__filter}>
-                    
+                    <FilterMenu filterMenu={filterMenu} />
                 </div>
                 <div className={styles.home_page__carousel}>
-
+                    <div className={styles.featured_meal}>
+                        <Image
+                            src={'/images/featured-meal-placeholder.png'}
+                            alt={'featured-meal-placeholder'}
+                            width={330}
+                            height={250}
+                        />
+                        <div className={styles.featured_meal__details}>
+                            <h2>Meal Name</h2>
+                            <p>Torem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <div className={styles.info}>
+                                <span>25 MIN</span>
+                                <div/>
+                                <span>EASY</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.featured_meal__dots}>
+                        <div/>
+                        <div/>
+                        <div/>
+                    </div>
                 </div>
                 <div className={styles.home_page__categories}>
-
+                    <h2>Meal Categories</h2>
+                    <div className={styles.meal_categories}>
+                        <Image
+                            src={'/images/meal-category-placeholder.png'}
+                            alt={'meal-category-placeholder.png'}
+                            width={155}
+                            height={170}
+                        />
+                    </div>
                 </div>
                 <Button
                     href='/game'
