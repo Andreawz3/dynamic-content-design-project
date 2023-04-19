2e8cc56134ffcb8a2f558cea69752fd729b3f6ac
lance-malaga <lmalaga@my.bcit.ca>
2023-04-19 02:21:17 -0700

    fix responsiveness and style close icon

--- public/icons/icons.js
+++ public/icons/icons.js
@@ -51,7 +51,7 @@ export function BackArrowIcon() {
 export function CloseIcon() {
     return (
         <>
-            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className={styles.close_icon} width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M11 1L1 11" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
             <path d="M11 11L1 1" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
