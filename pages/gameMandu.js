0dc2904e408555e568a48c4cf078c6e313a3b9d5
Andrea Wu <andrea.wzg@gmail.com>
2023-04-23 15:59:01 -0700

    add mandu, miso pasta and rissoto game pages and style files

---
+++ pages/gameMandu.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/GameMandu.module.css';
+
+
+export default function gameMandu() {
+    return (
+        <>
+            <h1>Mandu Game</h1>
+        </>
+    )
+}
---
+++ pages/gameMisoPasta.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/GameMisoPasta.module.css';
+
+
+export default function gameMisoPasta() {
+    return (
+        <>
+            <h1>Miso Pasta Game</h1>
+        </>
+    )
+}
