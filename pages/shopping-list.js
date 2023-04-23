f07ffefa0bf7eccef6c3c121f04782e3e65a882f
lance-malaga <lmalaga@my.bcit.ca>
2023-04-23 13:08:34 -0700

    fix checkbox styling

--- pages/shopping-list.js
+++ pages/shopping-list.js
@@ -4,13 +4,15 @@ import NavBar from '@/components/NavBar'
 import NavMenu from '@/components/NavMenu'
 import Search from '@/components/Search'
 import TopNav from '@/components/TopNav'
+import { shopping_list } from '@/data/shopping-list'
 import useNavMenu from '@/hooks/navmenu'
 
 import styles from '@/styles/ShoppingList.module.css'
 
 export default function ShoppingList() {
-    const listCategories = ["Recently Added", "By Recipe"];
     const {showMenu, setShowMenu} = useNavMenu();
+    const listCategories = ["Recently Added", "By Recipe"];
+    const shoppingList = shopping_list;
 
     return (
         <>
@@ -30,7 +32,21 @@ export default function ShoppingList() {
 					/>
                 </div>
 				<div className={styles.list_page__content}>
-                    
+                    {shoppingList.map((data, index) => 
+                        <div 
+                            className={styles.shopping_list}
+                            style={{borderLeft: data.id == 1 ? "4px solid var(--color-teal)" : "4px solid var(--color-orange)"}}
+                        >
+                            <div className={styles.shopping_list__content}>
+                                <p>{data.quantity} {data.ingredient}</p>
+                                <p>saved from {data.meal} recipe</p>
+                            </div>
+                            <div className={styles.form_group}>
+                                <input type="checkbox" id={data.ingredient}/>
+                                <label for={data.ingredient} />
+                            </div>
+                        </div>
+                    )}
                 </div>
             </main>
             <NavBar/>
