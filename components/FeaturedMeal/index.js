559889eb0693d5aae0d72ea17893278aae1c15fe
lance-malaga <lmalaga@my.bcit.ca>
2023-05-15 10:50:09 -0700

    add links to featured meals

--- components/FeaturedMeal/index.js
+++ components/FeaturedMeal/index.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { inventory } from '@/data/recipes';
 
 import styles from './FeaturedMeal.module.css'
+import Link from 'next/link';
 
 export default function FeaturedMeal() {
 
@@ -11,23 +12,25 @@ export default function FeaturedMeal() {
 
     return (
         <>
-            <div className={styles.featured_meal}>
-                <Image
-                    src={featuredMeal[currentIndex].image}
-                    alt={featuredMeal[currentIndex].dishName}
-                    width={330}
-                    height={250}
-                />
-                <div className={styles.featured_meal__details}>
-                    <h2>{featuredMeal[currentIndex].dishName}</h2>
-                    <p>{featuredMeal[currentIndex].featuredDesc}</p>
-                    <div className={styles.info}>
-                        <span>{featuredMeal[currentIndex].prepTime.toUpperCase()} </span>
-                        <div/>
-                        <span>{featuredMeal[currentIndex].difficulty.toUpperCase()}</span>
+            <Link href={`/meal-recipe/${featuredMeal[currentIndex].slug}`}>
+                <div className={styles.featured_meal}>
+                    <Image
+                        src={featuredMeal[currentIndex].image}
+                        alt={featuredMeal[currentIndex].dishName}
+                        width={330}
+                        height={250}
+                    />
+                    <div className={styles.featured_meal__details}>
+                        <h2>{featuredMeal[currentIndex].dishName}</h2>
+                        <p>{featuredMeal[currentIndex].featuredDesc}</p>
+                        <div className={styles.info}>
+                            <span>{featuredMeal[currentIndex].prepTime.toUpperCase()} </span>
+                            <div/>
+                            <span>{featuredMeal[currentIndex].difficulty.toUpperCase()}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Link>
             <div className={styles.featured_meal__dots}>
                 {featuredMeal.map((value, index) => 
                     <div key={index} onClick={() => setCurrentIndex(index)} style={{backgroundColor: currentIndex === index ? "var(--color-avocado)" : "var(--color-light-green)"}} />
